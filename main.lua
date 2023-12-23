5b6dbb2cbe7d0fd5dedab3dcb467a2e436362dda
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-23 15:19:38 -0500

    1

--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
 -- Main controller for the Colony_Manager app
 
--- Placeholder code to ensure Lua environment is running properly
+local colonyDataModule = require("modules.colonyData")
+
 print("Colony Manager starting...")
 
--- For now, simply exit
+-- Fetch and print basic colony data
+-- The peripheral name passed here should match the actual name in the Minecraft world
+-- Replace 'colonyIntegrator_1' with the correct peripheral name
+-- INPUT_REQUIRED {Replace 'colonyIntegrator_1' with the actual name of your Colony Integrator peripheral}
+colonyDataModule.fetchBasicColonyData('colonyIntegrator_1')
+
 print("Exiting Colony Manager.")
--- modules/colonyData.lua
+++ modules/colonyData.lua
@@ -1,11 +1,38 @@
--- Module to interface with the minecolonies via the colonyIntegrator block
-
--- Placeholder code for the first task
+-- Interacting with the minecolonies mod via the colonyIntegrator block
 local colonyData = {}
 
-function colonyData.fetchBasicColonyData()
-    -- Placeholder for fetching basic colony data functionality
-    print("Fetching basic colony data...")
+function colonyData.fetchBasicColonyData(peripheralName)
+    local integrator = peripheral.wrap(peripheralName)
+    if not integrator then 
+        error("Could not wrap the colonyIntegrator peripheral. Is it correctly named and placed?")
+    end
+
+    -- Using the correct methods provided by the colonyIntegrator API
+    local colonyID = integrator.getColonyID()
+    local colonyName = integrator.getColonyName()
+    local happiness = integrator.getHappiness()
+    local amountOfCitizens = integrator.amountOfCitizens()
+    local maxOfCitizens = integrator.maxOfCitizens()
+    local amountOfGraves = integrator.amountOfGraves()
+    local amountOfConstructionSites = integrator.amountOfConstructionSites()
+
+    -- Prepare a table with basic colony data
+    local data = {
+        colonyID = colonyID,
+        colonyName = colonyName,
+        happiness = happiness,
+        citizenCount = amountOfCitizens,
+        citizenCapacity = maxOfCitizens,
+        gravesCount = amountOfGraves,
+        constructionSitesCount = amountOfConstructionSites,
+    }
+
+    -- Print out the data for inspection
+    for k, v in pairs(data) do
+        print(k .. ": " .. tostring(v))
+    end
+
+    return data
 end
 
 return colonyData
