e8da9ef6c0fe3fffbc6ff9bd028bc96e8692371f
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-23 15:43:12 -0500

    1

--- main.lua
+++ main.lua
@@ -9,8 +9,7 @@ print("Colony Manager starting...")
 
 -- Fetch and print basic colony data
 -- The peripheral name passed here should match the actual name in the Minecraft world
--- Replace 'colonyIntegrator_1' with the correct peripheral name
--- INPUT_REQUIRED {Replace 'colonyIntegrator_1' with the actual name of your Colony Integrator peripheral}
+-- INPUT_REQUIRED {Replace 'colonyIntegrator_1' with the correct peripheral name}
 colonyDataModule.fetchBasicColonyData('colonyIntegrator_1')
 
 -- Define the monitoring functions
