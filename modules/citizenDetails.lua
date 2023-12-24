56471ff432f5e103853c6f6c23b8ba9be12a7377
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-23 19:03:11 -0500

    Update citizenDetails.lua

--- modules/citizenDetails.lua
+++ modules/citizenDetails.lua
@@ -1,4 +1,4 @@
--- Module for fetching detailed citizen information including extra details
+-- Module for fetching detailed citizen information
 local citizenDetails = {}
 
 function citizenDetails.fetchAllCitizenDetails()
@@ -11,37 +11,20 @@ function citizenDetails.fetchAllCitizenDetails()
     local detailedCitizenInfo = {}
     
     for _, citizen in pairs(citizens) do
-        local extraDetails = citizenDetails.fetchExtraCitizenDetails(citizen.id)
+        -- Extracting the bed position, food saturation, and happiness directly
         table.insert(detailedCitizenInfo, {
             id = citizen.id,
             name = citizen.name,
             age = citizen.age,
             gender = citizen.gender,
             location = citizen.location,
-            bedPosition = extraDetails.bedPosition,
-            foodSaturation = extraDetails.foodSaturation,
-            happiness = extraDetails.happiness
+            bedPosition = citizen.bedPos, -- directly using bedPos from citizen
+            foodSaturation = citizen.saturation, -- directly using saturation from citizen
+            happiness = citizen.happiness -- directly using happiness from citizen
         })
     end
     
     return detailedCitizenInfo
-end
-
-function citizenDetails.fetchExtraCitizenDetails(citizenID)
-    local integrator = peripheral.find("colonyIntegrator")
-    if not integrator then
-        error("Could not find a colonyIntegrator peripheral.")
-    end
-
-    local bedPosition = integrator.getBedPosition(citizenID)
-    local foodSaturation = integrator.getFoodSaturation(citizenID)
-    local happiness = integrator.getCitizenHappiness(citizenID)
-
-    return {
-        bedPosition = bedPosition,
-        foodSaturation = foodSaturation,
-        happiness = happiness
-    }
 end
 
 function citizenDetails.logCitizenDetails()
@@ -53,6 +36,9 @@ function citizenDetails.logCitizenDetails()
         print("Age: " .. citizen.age)
         print("Gender: " .. citizen.gender)
         print("Location: X=" .. citizen.location.x .. " Y=" .. citizen.location.y .. " Z=" .. citizen.location.z)
+        print("Bed position: X=" .. citizen.bedPosition.x .. " Y=" .. citizen.bedPosition.y .. " Z=" .. citizen.bedPosition.z)
+        print("Food Saturation: " .. citizen.foodSaturation)
+        print("Happiness: " .. citizen.happiness)
         print("------")
     end
 end
