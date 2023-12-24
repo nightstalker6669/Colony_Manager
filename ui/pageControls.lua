85a496bfee92b70fe4bc88334d775b574ad17fde
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-23 23:12:32 -0500

    Update pageControls.lua

--- ui/pageControls.lua
+++ ui/pageControls.lua
@@ -3,11 +3,18 @@ local uiHelpers = require("ui.uiHelperFunctions")
 local pageControls = {}
 
 function pageControls.showPageControls(monitor, currentPage, totalPages, y)
-    local monitorWidth, _ = monitor.getSize()
+    local monitorWidth, monitorHeight = monitor.getSize()
+    -- Initialize y to the last line of the monitor if it's nil
+    y = y or monitorHeight
+
     if currentPage > 0 then
+        -- Adding checks to ensure we do not call drawButton with nil for y
+        assert(type(y) == "number", "Y coordinate must be a number for page controls.")
         uiHelpers.drawButton(monitor, 2, y, "< Prev", "prevPage")
     end
     if currentPage < totalPages - 1 then
+        -- Adding checks to ensure we do not call drawButton with nil for y
+        assert(type(y) == "number", "Y coordinate must be a number for page controls.")
         uiHelpers.drawButton(monitor, monitorWidth - 8, y, "Next >", "nextPage")
     end
 end
