09151264bc406ebc36f8126282685d130f24f01b
nightstalker6669 <nightstalker6669@yahoo.com>
2023-12-27 22:50:01 -0500

    17

--- ui/citizenDisplay.lua
+++ ui/citizenDisplay.lua
@@ -6,54 +6,54 @@ local citizenDisplay = {}
 local currentPage = 0
 
 function citizenDisplay.showCitizens(citizens, monitor, currentPage)
-    local y = 2
-    local citizensPerPage = 4
-    local startIndex = currentPage * citizensPerPage + 1
-    local endIndex = math.min(startIndex + citizensPerPage - 1, #citizens)
-    local totalPages = math.ceil(#citizens / citizensPerPage)
+  local y = 2
+  local citizensPerPage = 4
+  local startIndex = currentPage * citizensPerPage + 1
+  local endIndex = math.min(startIndex + citizensPerPage - 1, #citizens)
+  local totalPages = math.ceil(#citizens / citizensPerPage)
 
-    for i = startIndex, endIndex do
-        local citizen = citizens[i]
-        y = citizenDisplay.drawCitizenDetails(citizen, monitor, y)
-    end
+  for i = startIndex, endIndex do
+    local citizen = citizens[i]
+    y = citizenDisplay.drawCitizenDetails(citizen, monitor, y)
+  end
 
-    local _, monitorHeight = monitor.getSize()
-    y = monitorHeight - 1
-    if totalPages > 1 then
-        pageControls.showPageControls(monitor, currentPage, totalPages, y)
-    end
+  local _, monitorHeight = monitor.getSize()
+  y = monitorHeight - 1
+  if totalPages > 1 then
+    pageControls.showPageControls(monitor, currentPage, totalPages, y)
+  end
 end
 
 function citizenDisplay.incrementPage()
-    currentPage = currentPage + 1
+  currentPage = currentPage + 1
 end
 
 function citizenDisplay.drawCitizenDetails(citizen, monitor, startY)
-    local y = startY
-    monitor.setCursorPos(1, y)
-    monitor.setTextScale(0.5)
-    monitor.write('Name: ' .. citizen.name)
-    y = y + 1
-    monitor.setCursorPos(1, y)
-    monitor.write('Age: ' .. citizen.age)
-    y = y + 1
-    monitor.setCursorPos(1, y)
-    monitor.write('Job: ' .. (citizen.job or "N/A"))
-    y = y + 1
-    monitor.setCursorPos(1, y)
-    monitor.write('Location: X=' .. citizen.location.x .. ' Y=' .. citizen.location.y .. ' Z=' .. citizen.location.z)
-    y = y + 1
-    monitor.setCursorPos(1, y)
-    monitor.write('Bed Position: X=' .. citizen.bedPosition.x .. ' Y=' .. citizen.bedPosition.y .. ' Z=' .. citizen.bedPosition.z) -- INPUT_REQUIRED {Add implementation for getting citizen's bed position}
-    y = y + 1
-    monitor.setCursorPos(1, y)
-    monitor.write('Food Saturation: ' .. citizen.foodSaturation) -- INPUT_REQUIRED {Add implementation for getting citizen's food saturation}
-    y = y + 1
-    monitor.setCursorPos(1, y)
-    monitor.write('Happiness: ' .. citizen.happiness) -- INPUT_REQUIRED {Add implementation for getting citizen's happiness}
-    y = y + 2
+  local y = startY
+  monitor.setCursorPos(1, y)
+  monitor.setTextScale(0.5)
+  monitor.write('Name: ' .. citizen.name)
+  y = y + 1
+  monitor.setCursorPos(1, y)
+  monitor.write('Age: ' .. citizen.age)
+  y = y + 1
+  monitor.setCursorPos(1, y)
+  monitor.write('Job: ' .. (citizen.job or "N/A"))
+  y = y + 1
+  monitor.setCursorPos(1, y)
+  monitor.write('Location: X=' .. citizen.location.x .. ' Y=' .. citizen.location.y .. ' Z=' .. citizen.location.z)
+  y = y + 1
+  monitor.setCursorPos(1, y)
+  monitor.write('Bed Position: X=' .. citizen.bedPosition.x .. ' Y=' .. citizen.bedPosition.y .. ' Z=' .. citizen.bedPosition.z) // INPUT_REQUIRED {Add implementation for getting citizen's bed position}
+  y = y + 1
+  monitor.setCursorPos(1, y)
+  monitor.write('Food Saturation: ' .. citizen.foodSaturation) // INPUT_REQUIRED {Add implementation for getting citizen's food saturation}
+  y = y + 1
+  monitor.setCursorPos(1, y)
+  monitor.write('Happiness: ' .. citizen.happiness) // INPUT_REQUIRED {Add implementation for getting citizen's happiness}
+  y = y + 2
 
-    return y
+  return y
 end
 
 return citizenDisplay
--- ui/navigation.lua
+++ ui/navigation.lua
@@ -4,42 +4,39 @@ local uiHelpers = require("ui.uiHelperFunctions")
 
 local actionBindings = {}
 local navigation = {}
-local TAB_START_LINE = 3
 
 function navigation.init(monitor, actions)
-    local tabs = {
-        { title: "HOME", action = actions.showHomePage },
-        { title: "CITIZENS", action = actions.showCitizenDetailsPage },
-        { title: "STATISTICS", action = actions.showStatisticsPage },
-        { title: "SETTINGS", action = actions.showSettingsPage }
-    }
-    -- SET TEXT SCALE -- ensure text size is set correctly before drawing tabs.
-    monitor.setTextScale(0.5)
-    navigation.drawTabs(monitor, TAB_START_LINE, tabs) -- pass tabs to the drawTabs function
+  local tabs = {
+    { title = 'HOME', action = actions.showHomePage },
+    { title = 'CITIZENS', action = actions.showCitizenDetailsPage },
+    { title = 'STATISTICS', action = actions.showStatisticsPage },
+    { title = 'SETTINGS', action = actions.showSettingsPage }
+  }
+  -- SET TEXT SCALE -- ensure text size is set correctly before drawing tabs.
+  monitor.setTextScale(0.5)
+  navigation.drawTabs(monitor, TAB_START_LINE, tabs) -- pass tabs to the drawTabs function
 end
 
 function navigation.drawTabs(monitor, startLine, tabList)
-    monitor.clear()
-    local x = 1
-    for _, tab in ipairs(tabList) do
-        if not uiHelpers.isButtonDrawn(tab["title"]) then
-            uiHelpers.drawButton(monitor, x, startLine, tab["title"], tab["title"])
-            x = x + string.len(tab["title"]) + 3
-            local monitorWidth, _ = monitor.getSize()
-            if x >= monitorWidth then
-                x = 1
-                startLine = startLine + 1
-            end
-        end
+  monitor.clear()
+  local x = 1
+  for _, tab in ipairs(tabList) do
+    if not uiHelpers.isButtonDrawn(tab["title"]) then
+      uiHelpers.drawButton(monitor, x, startLine, tab["title"], tab["title"])
+      x = x + string.len(tab["title"]) + 3
+      local monitorWidth, _ = monitor.getSize()
+      if x >= monitorWidth then
+        x = 1
+        startLine = startLine + 1
+      end
     end
+  end
 end
 
--- Rest of the navigation.lua code...
-
 navigation.bindActions = function(bindings)
-    for actionName, action in pairs(bindings) do
-        actionBindings[actionName:upper()] = action
-    end
+  for actionName, action in pairs(bindings) do
+    actionBindings[actionName:upper()] = action
+  end
 end
 
 return navigation
